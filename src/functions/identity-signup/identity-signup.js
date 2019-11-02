6409b9a35207776cdcc1148cc2777aa2f203333d
Aditya Pratap <adityapratap7344@gmail.com>
2019-11-02 05:50:44 +0530

    modified identity-signup function to create a new user in our faunadb database on sign up

--- src/functions/identity-signup/identity-signup.js
+++ src/functions/identity-signup/identity-signup.js
@@ -5,6 +5,15 @@
 // https://www.netlify.com/blog/2019/02/21/the-role-of-roles-and-how-to-set-them-in-netlify-identity/
 // https://www.netlify.com/docs/functions/#identity-and-functions
 
+/* Import faunaDB sdk */
+const faunadb = require('faunadb')
+
+/* configure faunaDB Client with our secret */
+const q = faunadb.query
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SERVER_SECRET
+})
+
 exports.handler = async function(event, context) {
   const data = JSON.parse(event.body)
   const { user } = data
@@ -17,12 +26,37 @@ exports.handler = async function(event, context) {
     user_metadata: {
       ...user.user_metadata, // append current user metadata
       createdAt: user.created_at,
-      email: user.email,
+      
       id: user.id,
     }
   }
-  return {
-    statusCode: 200,
-    body: JSON.stringify(responseBody)
+
+  const userData = {
+    id: user.id,
+    created_at: user.created_at,
+    email: user.email,
+    name: user.user_metadata.full_name
   }
+
+  const newUser =  {
+    data: userData
+  }
+
+
+  return  client.query(q.Create(q.Ref('classes/users'), newUser))
+    .then((response) => {
+      console.log('success', responseBody)
+      /* Success! return the response with statusCode 200 */
+      return {
+        statusCode: 200,
+        body: JSON.stringify(responseBody)
+      }
+    }).catch((error) => {
+      console.log('error', error)
+      /* Error! return the error with statusCode 400 */
+      return {
+        statusCode: 400,
+        body: JSON.stringify(error)
+      }
+    })
 }
