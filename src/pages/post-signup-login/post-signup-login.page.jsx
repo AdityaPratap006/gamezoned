b1452ec1ac2974041363cb380719645f0d8663da
Aditya Pratap <adityapratap7344@gmail.com>
2019-11-05 00:29:08 +0530

    fixed up authentication and routing, added signup-login page  and post-signup-login page

--- src/pages/post-signup-login/post-signup-login.page.jsx
+++ src/pages/post-signup-login/post-signup-login.page.jsx
@@ -16,7 +16,7 @@ const PostSignupLoginPage = ({currentUser, setCurrentUser}) => {
     return (
         <div>
             <h3>You've Signed up!</h3>
-            <h2>Now Please Login again to confirm</h2> 
+            <h2>Now Please Log Out and Log In again to confirm</h2> 
             <AuthStatusView/>
         </div>
     )
