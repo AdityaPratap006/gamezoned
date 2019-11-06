e2e976060ba8d846f420c8333d8d0ebe10db6c90
Aditya Pratap <adityapratap7344@gmail.com>
2019-11-07 00:15:59 +0530

    fetching likes by user data in home page component and storing it in our redux state

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -5,11 +5,11 @@ import { connect } from 'react-redux';
 //import  { useIdentityContext  } from 'react-netlify-identity-widget';
 
 import { setAllPosts } from '../../redux/all_posts/all-posts.actions';
-
+import { setLikesByUser } from '../../redux/likes_by_user/likes_by_user.actions';
 
 import PostsContainer from '../../components/posts-container/posts-container.component';
 
-const HomePage = ({currentUser, setAllPosts, all_posts}) => {
+const HomePage = ({currentUser, setAllPosts, setLikesByUser, all_posts}) => {
 
     const fetchAllPosts = () => {
 
@@ -18,16 +18,34 @@ const HomePage = ({currentUser, setAllPosts, all_posts}) => {
             })
             .then(res => res.json())
     }
+
+    const fetchAllLikesByCurrentUser = (userId) => {
+        return fetch('/.netlify/functions/all-posts-fetch',{
+            method:'POST',
+            body: JSON.stringify({
+                id:userId
+            })
+        })
+        .then(res => res.json())
+    }
     
     useEffect(() => {
        
         fetchAllPosts()
-        .then(data => {
-            setAllPosts(data)
+        .then(postsData => {
+            
+
+            fetchAllLikesByCurrentUser(currentUser.faunadbUserId)
+            .then(likesData => {
+                setAllPosts(postsData);
+                setLikesByUser(likesData);
+            })
         })
         .catch(err => console.log(err))
 
-    }, [setAllPosts])
+
+
+    }, [setAllPosts, setLikesByUser, currentUser.faunadbUserId])
     
     return (
         <div className='home-page'>
@@ -38,11 +56,13 @@ const HomePage = ({currentUser, setAllPosts, all_posts}) => {
 
 const mapStateToProps = state => ({
     currentUser: state.user.currentUser,
-    all_posts: state.all_posts.all_posts
+    all_posts: state.all_posts.all_posts,
+    likes_by_user: state.likes_by_user.likes_by_user
 })
 
 const mapDispatchToProps = dispatch => ({
-    setAllPosts: posts => dispatch(setAllPosts(posts))
+    setAllPosts: posts => dispatch(setAllPosts(posts)),
+    setLikesByUser: likes => dispatch(setLikesByUser(likes))
 })
 
 export default connect(
